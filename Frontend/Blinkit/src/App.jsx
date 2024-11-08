869a63c1f20d4ac73f926785efa73cb01f15230f
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2024-11-08 23:05:45 +0545

    routes added

--- Frontend/Blinkit/src/App.jsx
+++ Frontend/Blinkit/src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import ProductDetails from './components/ProductDetails';
 import ProductGrid from './components/ProductCard'
 import Footer from './components/Footer'
 import Layout from './components/Layout'
+import { BrowserRouter, Route, Routes } from  'react-router-dom';
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import SignUp from './Pages/SignUp'
 
 function App() {
 
 
   return (
     <>
-     <Layout>
-     <ProductGrid/>
-     </Layout>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<SignUp/>}></Route>
+      <Route path="/productdetail" element={<ProductDetails/>}></Route>
+     </Routes>
+     </BrowserRouter>
    
     </>
   )
---
+++ Frontend/Blinkit/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import ProductGrid from '../components/ProductCard'
+
+const Home = () => {
+  return (
+    <Layout>
+     <ProductGrid/>
+     </Layout>
+    
+  )
+}
+
+export default Home
